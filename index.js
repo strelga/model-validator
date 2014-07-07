f3f97c0ab747d12b4845cccb168da8ebb5b1c5b4
grigory <strelga@gmail.com>
2014-07-07 17:18:39 +0400

    Schema.prototype.add tested with mixed fields. Works well.

--- index.js
+++ index.js
@@ -1,50 +1,3 @@
-// var paths = {};
-
-// var isObject = function (arg) {
-//   return '[object Object]' == toString.call(arg);
-// }
-
-// function addToPaths(obj, prefix) {
-// 	prefix = prefix || '';
-
-// 	var keys = Object.keys(obj);
-
-// 	for (var i = 0, l = keys.length; i < l; i++) {
-// 		var key = keys[i];
-
-// 		if (isObject(obj[key]) && (!obj[key].type || obj[key].type.type)) {
-// 			if (Object.keys(obj[key]).length) {
-// 		        // nested object { last: { name: String } }
-// 		        addToPaths(obj[key], prefix + key + '.');
-// 			} else {
-// 				paths[prefix + key] = obj[key]; // mixed type
-// 		    }
-// 		} else {
-// 			paths[prefix + key] = obj[key];
-// 		}
-// 	}
-// }
-
-// var schema = {
-// 	userId1 : { type : String, required : true },
-// 	userId2 : { type : String, required : true },
-// 	userIds : [String],
-// 	attrs : {
-// 		cntId : { type : String, required : true },
-// 		cntType : { type : Number, required : true },
-// 		user1 : {
-// 			voteType : { type : Number, required : true }
-// 		},
-// 		user2 : {
-// 			voteType : { type : Number, required : true }
-// 		}
-// 	}
-// };
-
-// addToPaths(schema);
-
-// console.log(paths);
-
 "use strict";
 
 var isObject = function (arg) {
@@ -52,147 +5,160 @@ var isObject = function (arg) {
 }
 
 var Schema = module.exports = function (conditions) {
-	this.paths = {};
-	
-	this.hasMixed = false;
-	this.mixedPaths = {};
+    this.paths = {};
+    
+    this.hasMixed = false;
+    this.mixedPaths = {};
 
-	this.constraints = {};
+    this.constraints = {};
 
-	this.add(conditions);
+    this.add(conditions);
 
-	this.cast();
+    this.cast();
 };
 
-// Schema.prototype.add = function (obj, prefix) {
-// 	prefix = prefix || '';
-
-// 	if (!isObject(obj)) {
-// 		obj = {};
-// 	}
-
-// 	var keys = Object.keys(obj);
-
-// 	for (var i = 0, l = keys.length; i < l; i++) {
-// 		var key = keys[i];
-
-// 		if (isObject(obj[key]) && (!obj[key].type || obj[key].type.type) && (Object.keys(obj[key]).length)) {
-// 		    this.add(obj[key], prefix + key + '.');
-// 		} else {
-// 			this.paths[prefix + key][conditions] = obj[key];
-// 		}
-// 	}
-// };
-
 var isCondition = function (element) {
-	return !isObject(element) || (element.type && !element.type.type) || !(Object.keys(element).length);
+    return !isObject(element) || (element.type && !element.type.type) || !(Object.keys(element).length);
 };
 
 var hasMixedField = function (element) {
-	return !!(element.mixed);
+    return !!(element.mixed);
 }
 
-/**
- * [query] Transforms the object of conditions to the object of paths.
- * 
- * @param {Object} obj The object of conditions.
- * @return {Object} The object of paths.
- */
-// Schema.prototype.add = function (obj) {
-// 	var paths = {};
-
-// 	if (!isObject(obj)) {
-// 		obj = {};
-// 	}
-
-// 	for (var key in obj) {
-// 		if (isCondition(obj[key])) {
-// 			if (hasMixedField(obj)) {
-
-// 			} else {
-// 				paths[key] = {
-// 					conditions : obj[key],
-// 					pathArray : [key]
-// 				};
-// 			}
-// 		} else {
-// 			var subpaths = this.add(obj[key]);
-
-// 		    for (var subPathKey in subpaths) {
-// 			    var pathKey = key + '.' + subPathKey;
-
-// 			    subpaths[subPathKey]['pathArray'].unshift(key)
-// 		    	paths[pathKey] = {
-// 		    		conditions : subpaths[subPathKey]['conditions'],
-// 		    		pathArray : subpaths[subPathKey]['pathArray']
-// 		    	}
-// 		    }
-// 		}
-// 	}
-
-// 	return paths;
-// };
-
 var makePathString = function (prefixString, key) {
-	return (prefixString) 
-		? prefixString + '.' + key
-		: key;
+    return (prefixString) 
+        ? prefixString + '.' + key
+        : key;
 };
 
 var makePathArray = function (prefixArray, key) {
-	var pathArray = prefixArray.slice();
-	pathArray.push(key);
-	
-	return pathArray;
+    var pathArray = prefixArray.slice();
+    pathArray.push(key);
+    
+    return pathArray;
 }
 
-Schema.prototype.add = function (obj, prefix, mixedPrefixString) {
-	if (!prefix) {
-		prefix = {
-			string : undefined,
-			array : []
-		}
-	}
+/**
+ * Gets object of conditions and transformes it into set of regular paths
+ * and mixed paths, changes variables this.paths, this.mixedPaths and sets
+ * this.hasMixed to true if there are mixed fields in conditions.
+ *
+ * example:
+ * obj = {
+ *   field1 : {type : "mixed", mixed : {
+ *           votes : {
+ *               cntId : {type : String, required : true},
+ *               cntType : {type : Number, required : true},
+ *               attrs : {type : "mixed", mixed : {
+ *                       pluses : {
+ *                           plus : {type : Boolean, required : true}
+ *                       },
+ *                       minuses : {
+ *                           minus : {type : Boolean, required : true}
+ *                       }
+ *                   }
+ *               }
+ *           },
+ *           connections : {
+ *               connId : {type : String, required : true} 
+ *           }
+ *       }
+ *   }
+ *
+ * this.mixedPaths = { 'field1.votes': 
+ *  { 'field1.cntId': 
+ *     { conditions: { type: [Function: String], required: true },
+ *       pathArray: [ 'field1', 'cntId' ] },
+ *    'field1.cntType': 
+ *     { conditions: { type: [Function: Number], required: true },
+ *       pathArray: [ 'field1', 'cntType' ] } },
+ * 'field1.votes.attrs.pluses': 
+ *  { 'field1.attrs.plus': 
+ *     { conditions: { type: [Function: Boolean], required: true },
+ *       pathArray: [ 'field1', 'attrs', 'plus' ] } },
+ * 'field1.votes.attrs.minuses': 
+ *  { 'field1.attrs.minus': 
+ *     { conditions: { type: [Function: Boolean], required: true },
+ *       pathArray: [ 'field1', 'attrs', 'minus' ] } },
+ * 'field1.connections': 
+ *  { 'field1.connId': 
+ *     { conditions: { type: [Function: String], required: true },
+ *       pathArray: [ 'field1', 'connId' ] } } }
+ * 
+ * @param {Object} obj                The object of conditions.
+ * @param {Object} prefix             The object containing prefix array and prefix string.
+ *                                      prefix = { string : 'field1.field2', array : ['field1', 'field2'] }
+ * @param {Object} mixedPrefixStrings The object containing full prefix string and current prefix string
+ *                                    for plain mixed fields and nested mixed fields.
+ *                                      mixedPrefixStrings = {
+ *                                          current : 'field1.mixed1',
+ *                                          full : 'field1.mixed1.field2'
+ *                                      }
+ */
+Schema.prototype.add = function (obj, prefix, mixedPrefixStrings) {
+    if (!prefix) {
+        prefix = {
+            string : undefined,
+            array : []
+        }
+    }
 
-	for (var key in obj) {
-		if (isCondition(obj[key])) {
-			if (hasMixedField(obj[key])) {
-				// there is the field 'mixed' in obj[key]
-				this.hasMixed = true;
+    if (!mixedPrefixStrings) {
+        mixedPrefixStrings = {
+            current : '',
+            full : ''
+        };
+    }
 
-				for (var mixedKey in obj[key].mixed) {
-					var nextMixedPrefixString = makePathString(makePathString(prefix.string, key), mixedKey);
-					var nextPrefix = {
-						string : makePathString(prefix.string, key),
-						array : makePathArray(prefix.array, key)
-					};
+    for (var key in obj) {
+        if (isCondition(obj[key])) {
+            if (hasMixedField(obj[key])) {
+                // there is the field 'mixed' in obj[key]
+                this.hasMixed = true;
 
-					this.add(obj[key]['mixed'][mixedKey], nextPrefix, nextMixedPrefixString);
-				}
-			} else {
-				// there is no field 'mixed' in obj[key] and obj[key] is just a condition
-				var path = {
-					conditions : obj[key],
-					pathArray : makePathArray(prefix.array, key)
-				};
+                for (var mixedKey in obj[key].mixed) {
+                    var nextMixedPrefixStrings = {};
+                    
+                    nextMixedPrefixStrings.current = makePathString(makePathString(mixedPrefixStrings.full, key), mixedKey);
+                    nextMixedPrefixStrings.full = nextMixedPrefixStrings.current;
+                    
+                    var nextPrefix = {
+                        string : makePathString(prefix.string, key),
+                        array : makePathArray(prefix.array, key)
+                    };
 
-				if (mixedPrefixString) {
-					this.mixedPaths[mixedPrefixString] = path;
-				} else {
-					var pathString = makePathString(prefix.string, key);
-					this.paths[pathString] = path;
-				}
-			}
-		} else {
-			// obj[key] is just another nested object, go deeper through it
-			var nextPrefix = {
-				string : makePathString(prefix.string, key),
-				array : makePathArray(prefix.array, key)
-			}
+                    this.add(obj[key]['mixed'][mixedKey], nextPrefix, nextMixedPrefixStrings);
+                }
+            } else {
+                // there is no field 'mixed' in obj[key] and obj[key] is just a condition
+                var path = {
+                    conditions : obj[key],
+                    pathArray : makePathArray(prefix.array, key)
+                };
 
-			this.add(obj[key], nextPrefix, mixedPrefixString);
-		}
-	}
+                var pathString = makePathString(prefix.string, key);
+
+                if (mixedPrefixStrings.current) {
+                    if (!this.mixedPaths[mixedPrefixStrings.current]) {
+                        this.mixedPaths[mixedPrefixStrings.current] = {};
+                    }
+                    this.mixedPaths[mixedPrefixStrings.current][pathString] = path;
+                } else {
+                    this.paths[pathString] = path;
+                }
+            }
+        } else {
+            // obj[key] is just another nested object, go deeper through it
+            var nextPrefix = {
+                string : makePathString(prefix.string, key),
+                array : makePathArray(prefix.array, key)
+            }
+
+            mixedPrefixStrings.full = makePathString(mixedPrefixStrings.full, key);
+
+            this.add(obj[key], nextPrefix, mixedPrefixStrings);
+        }
+    }
 };
 
 
@@ -210,41 +176,41 @@ Schema.prototype.validate = function (obj) {
 /**
  * Casts paths with conditions (this.paths) to paths with constraints object (this.constraints).
  *
- *	paths = {
- *		'field1' : {
- *			conditions : {type : String, required : true}
- *			pathArray : ['field1']
- *		},
- *		'field2.field3' : {
- *			conditions : {type : Boolean, default : true},
- *			pathArray : ['field2', 'field3']
- *		}
- *	}
- *	
- *	-> casts to ->
- *	
- *	constraints = {
- *		'field1' : {
- *			constraints : {
- *				default : undefined,
- *				required : true,
- *				check : function (element) {
- *					return isString(element);
- *				}
- *			},
- *			pathArray : ['field1']
- *		},
- *		'field2.field3' : {
- *			constraints : {
- *				default : true,
- *				required : false,
- *				check : function (element) {
- *					return isBoolean(element);
- *				}
- *			},
- *			pathArray : ['field2', 'field3']
- *		}
- *	}
+ *  paths = {
+ *      'field1' : {
+ *          conditions : {type : String, required : true}
+ *          pathArray : ['field1']
+ *      },
+ *      'field2.field3' : {
+ *          conditions : {type : Boolean, default : true},
+ *          pathArray : ['field2', 'field3']
+ *      }
+ *  }
+ *  
+ *  -> casts to ->
+ *  
+ *  constraints = {
+ *      'field1' : {
+ *          constraints : {
+ *              default : undefined,
+ *              required : true,
+ *              check : function (element) {
+ *                  return isString(element);
+ *              }
+ *          },
+ *          pathArray : ['field1']
+ *      },
+ *      'field2.field3' : {
+ *          constraints : {
+ *              default : true,
+ *              required : false,
+ *              check : function (element) {
+ *                  return isBoolean(element);
+ *              }
+ *          },
+ *          pathArray : ['field2', 'field3']
+ *      }
+ *  }
  * 
  * @api private
  */
--- test/schema.test.js
+++ test/schema.test.js
@@ -1,21 +1,26 @@
 "use strict";
 
+var util = require('util');
+
 var Schema = require('../');
 
 describe('Schema class', function () {
-	var schema;
-
-	beforeEach(function () {
-		// schema = new Schema();
-	});
-
 	describe('#add', function () {
-		it('should parse conditions and transform them to paths', function () {
-			var schemaStub = {
-				paths : {}
+		var schemaStub;
+
+		beforeEach(function () {
+			var SchemaStub = function () {
+				this.paths = {};
+				this.hasMixed = false;
+				this.mixedPaths = {};
 			};
-			schemaStub.__proto__.add = Schema.prototype.add;
-			
+
+			SchemaStub.prototype.add = Schema.prototype.add;
+
+			schemaStub = new SchemaStub();
+		});
+
+		it('should parse conditions and transform them to paths', function () {
 			var conditions = {
 				field1 : { type : String, required : true },
 				field2 : {
@@ -26,7 +31,7 @@ describe('Schema class', function () {
 
 			Schema.prototype.add.call(schemaStub, conditions);
 
-			// console.log(paths);
+			// console.log(schemaStub.paths);
 
 			schemaStub.paths['field1'].conditions.should.match({type : String, required : true});
 			schemaStub.paths['field1'].pathArray.should.match(['field1']);
@@ -34,6 +39,76 @@ describe('Schema class', function () {
 			schemaStub.paths['field2.field3'].pathArray.should.match(['field2', 'field3']);
 			schemaStub.paths['field2.field4'].conditions.should.match({type : Number, required : true});
 			schemaStub.paths['field2.field4'].pathArray.should.match(['field2', 'field4']);
+		});
+
+		it('should parse conditions for mixed fields', function () {
+			var conditions = {
+				field1 : {
+					field2 : { 
+						type : 'mixed', mixed : {
+							votes : { 
+								cntId : {type : String, required : true},
+								cntType : {type : Number, required : true}
+							},
+							connections : {
+								connId : {type : String, required : true}
+							}	
+						}
+					}
+				}
+			};
+
+			Schema.prototype.add.call(schemaStub, conditions);
+
+			// console.log(util.inspect(schemaStub.mixedPaths, {depth : null}));
+
+			schemaStub.mixedPaths['field1.field2.votes']['field1.field2.cntId'].conditions.should.match({type : String, required : true});
+			schemaStub.mixedPaths['field1.field2.votes']['field1.field2.cntId'].pathArray.should.match(['field1', 'field2', 'cntId']);
+			schemaStub.mixedPaths['field1.field2.votes']['field1.field2.cntType'].conditions.should.match({type : Number, required : true});
+			schemaStub.mixedPaths['field1.field2.votes']['field1.field2.cntType'].pathArray.should.match(['field1', 'field2', 'cntType']);
+			schemaStub.mixedPaths['field1.field2.connections']['field1.field2.connId'].conditions.should.match({type : String, required : true});
+			schemaStub.mixedPaths['field1.field2.connections']['field1.field2.connId'].pathArray.should.match(['field1', 'field2', 'connId']);
+
+			schemaStub.hasMixed.should.be.ok;
+		});
+
+		it('should parse conditions for nested mixed fields', function () {
+			var conditions = {
+				field1 : {type : "mixed", mixed : {
+						votes : {
+							cntId : {type : String, required : true},
+							cntType : {type : Number, required : true},
+							attrs : {type : "mixed", mixed : {
+									pluses : {
+										plus : {type : Boolean, required : true}
+									},
+									minuses : {
+										minus : {type : Boolean, required : true}
+									}
+								}
+							}
+						},
+						connections : {
+							connId : {type : String, required : true} 
+						}
+					}
+				}
+			};
+
+			schemaStub.add(conditions);
+
+			// console.log(util.inspect(schemaStub.mixedPaths, {depth : null}));
+
+			schemaStub.mixedPaths['field1.votes']['field1.cntId'].conditions.should.match({type : String, required : true});
+			schemaStub.mixedPaths['field1.votes']['field1.cntId'].pathArray.should.match(['field1', 'cntId']);
+			schemaStub.mixedPaths['field1.votes']['field1.cntType'].conditions.should.match({type : Number, required : true});
+			schemaStub.mixedPaths['field1.votes']['field1.cntType'].pathArray.should.match(['field1', 'cntType']);
+			schemaStub.mixedPaths['field1.votes.attrs.pluses']['field1.attrs.plus'].conditions.should.match({type : Boolean, required : true});
+			schemaStub.mixedPaths['field1.votes.attrs.pluses']['field1.attrs.plus'].pathArray.should.match(['field1', 'attrs', 'plus']);
+			schemaStub.mixedPaths['field1.votes.attrs.minuses']['field1.attrs.minus'].conditions.should.match({type : Boolean, required : true});
+			schemaStub.mixedPaths['field1.votes.attrs.minuses']['field1.attrs.minus'].pathArray.should.match(['field1', 'attrs', 'minus']);
+			schemaStub.mixedPaths['field1.connections']['field1.connId'].conditions.should.match({type : String, required : true});
+			schemaStub.mixedPaths['field1.connections']['field1.connId'].pathArray.should.match(['field1', 'connId']);
 		});
 	});
 });
